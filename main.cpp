939fcd614d52b65b2e4552281d362a23df8426e5
Austin <austinfu113chn@hotmail.com>
2024-02-02 02:22:04 +0000

    added game path reading and setting

--- main.cpp
+++ main.cpp
@@ -3,24 +3,38 @@
 #include "./config.h"
 
 std::string getWD(){
-    std::string working_dir;
+    std::string game_dir;
     std::cout << "Plase Enter Game Path: \n";
-    std::cin >> working_dir;
+    std::cin >> game_dir;
 
-    return working_dir;
+    return game_dir;
 }
 
 int main(){
+    std::string game_dir;
+    std::ifstream game_path_file("./.game_path.txt");
+    getline(game_path_file, game_dir);
+    game_path_file.close();
+    if(game_dir.length()==0){
+        std::cout << "No Game Path is Found.\n";
+        std::cout << "Set Game Path Now? (yes/no; Defualt no)"<<std::endl;
+        std::string set_path_op;
+        std::cin >> set_path_op;
+        if(set_path_op=="yes"){
+            game_dir = getWD();
+            std::ofstream game_path_file("./.game_path.txt");
+            game_path_file << game_dir;
+            game_path_file.close();
+        }else{
+            return 0;
+        }
+    }
+    std::string init_path = game_dir + "config.ini";
+    
     int option;
-
     std::cout << "Server Options: \n 1. Mihoyo Server\n 2. Bilibili Server\n";
     std::cout << "Select Server: ";
     std::cin >> option;    
 
-    std::string working_dir;
-    std::string game_path;
-    std::string init_path = working_dir + "config.ini";
-    std::ifstream game_path_file("./.game_path.txt");
-    getline(game_path_file, game_path);
     
 }
